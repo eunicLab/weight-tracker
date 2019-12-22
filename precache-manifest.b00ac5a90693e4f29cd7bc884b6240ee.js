7a63cd9d8ddd07d59702aa11af4610cbb9f6ef88
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2019-12-22 08:54:46 +0100

    Updates

---
+++ precache-manifest.b00ac5a90693e4f29cd7bc884b6240ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aae33f43430e342c0e9ca62a1d7bf3d",
+    "url": "/weight-tracker2/index.html"
+  },
+  {
+    "revision": "e2380116a6d9399abd0e",
+    "url": "/weight-tracker2/static/css/main.ee1f9bcd.chunk.css"
+  },
+  {
+    "revision": "7f2ac6ed39e762147042",
+    "url": "/weight-tracker2/static/js/2.442306d4.chunk.js"
+  },
+  {
+    "revision": "e2380116a6d9399abd0e",
+    "url": "/weight-tracker2/static/js/main.c341ccf6.chunk.js"
+  },
+  {
+    "revision": "172aa3a65cd44d1c1317",
+    "url": "/weight-tracker2/static/js/runtime-main.76fb677c.js"
+  }
+]);
